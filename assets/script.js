f2df6967887d70ce4fb11a1d376284b1a1a2bbbf
Tim Mardis <mardistj96@yahoo.com>
2022-10-26 20:29:36 -0500

    Added 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ let searchButton = document.getElementById('search-button');
 
 function getApi() {
     let userSearch = document.getElementById("user-search").value
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=08f021b8984031642c1fe43ac7027511`;
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userSearch}&units=imperial&appid=08f021b8984031642c1fe43ac7027511`;
 
     fetch(requestUrl)
         .then(function (response) {
@@ -11,7 +11,21 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
+      getApi1()
       })
 };
+
+function getApi1() {
+    let userSearch = document.getElementById("user-search").value
+    var requestUrl1 = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&units=imperial&appid=910806b278b33f1777e94e6b5bddd6df`;
+
+    fetch(requestUrl1)
+        .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      })
+}
 
 searchButton.addEventListener('click', getApi);
